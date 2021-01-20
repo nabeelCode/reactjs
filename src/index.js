33db4e7597b2bfbf1122bbeca20923a3a9a2109d
nabeelCode <mail.nabeelmuhammed@gmail.com>
2021-01-20 17:54:49 +0530

    timer app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+//set countdown down time in seconds
+const SECONDS = 1000
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state= { seconds: SECONDS }
+    }
+
+    componentDidMount(){
+        setInterval(()=>this.tick(),1000)
+    }
+    tick(){
+        if(this.state.seconds > 0)
+        this.setState((state)=>({
+            seconds : state.seconds - 1
+        }))
+    }
+
+    render(){
+        return(
+            <div style={{textAlign: 'center'}}>
+                <h2>Timer Based on Seconds</h2>
+                <h3>
+                    { new Date(this.state.seconds * 1000).toISOString().substr(11,8) }
+                </h3>
+            </div>
+        )
+    }
+} 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        <App/>,
+        document.getElementById('root')
+    )
